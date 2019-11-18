a82e7f382b620ae181339d83520d5d4aabf3b1b6
Glendon Small <gsmall@sonovate.com>
2019-11-18 18:09:56 +0000

    heroku update

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/starboy'));
+app.use(express.static('./dist/datta-able-v7.2.2'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname, '/dist/starboy/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/datta-able-v7.2.2/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
